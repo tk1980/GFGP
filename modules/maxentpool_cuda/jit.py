8faf8239fd5c62172828fc6368e42b2fc6db1eb0
tk1980 <53114307+tk1980@users.noreply.github.com>
2019-10-29 01:51:20 +0900

    Delete jit.py

--- modules/maxentpool_cuda/jit.py
+++
@@ -1,3 +0,0 @@
-from torch.utils.cpp_extension import load
-maxentpool2d_cuda = load(name='maxentpool2d_cuda', sources=['maxentpool2d_cuda.cpp', 'maxentpool2d_cuda_kernel.cu'], verbose=True)
-help(maxentpool2d_cuda)
